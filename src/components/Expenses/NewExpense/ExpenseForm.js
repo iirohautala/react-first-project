7bed6a9ff801fe692dbf82659a40a836f97cecb8
Iiro Hautala <iirohautala@gmail.com>
2024-10-02 18:00:08 +0300

    fixed some errors

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -4,9 +4,9 @@ import './ExpenseForm.css';
 
 const ExpenseForm = () => {
 
-  const [enteredTitle, setEnteredTitle] = useState ('');
-  const [enteredAmount, setEnteredAmount] = useState ('');
-  const [enteredDate, setEnteredDate] = useState ('');
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = (event) => {
     console.log(event.target.value);
@@ -27,11 +27,11 @@ const ExpenseForm = () => {
       </div>
       <div className='new-expense__control'>
         <label>Amount</label>
-        <input type='number' min='0.01' step='0.01' />
+        <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
       </div>
       <div className='new-expense__control'>
         <label>Date</label>
-        <input type='date' min='2022-01-01' max='2024-12-31' />
+        <input type='date' min='2022-01-01' max='2024-12-31' onChange={dataChangeHandler}/>
       </div>
       <div className='new-expense__actions'>
         <button type='submit'>Add Expense</button>
