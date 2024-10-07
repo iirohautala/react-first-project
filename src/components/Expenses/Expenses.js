49956ff192671ef7457daf8c9149cbc19746ce06
Hautala <e2301986@edu.vamk.fi>
2024-10-07 13:52:57 +0300

    created filter components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,23 @@
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
-import React from 'react';
+import React, { useState } from 'react';
+import ExpensesFilter from './ExpensesFilter';
+
 
 const Expenses = (props) => {
+
+    const [filteredYear, setFilteredYear] = useState('2024');
+
+    const filterChangeHandler = (selectedYear) => {
+        //console.log('Expenses.js');
+        //console.log(selectedYear);
+        setFilteredYear(selectedYear);
+    }
+
     return (
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
